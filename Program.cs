f4d5ddd599581c19a6f1446675d9bddd6d8768cf
kaban <taymuraz.piliyev@mail.ru>
2024-10-20 14:47:52 +0300

    neocal v1

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Приветствие
+        Console.WriteLine("Добро пожаловать в турбокалькулятор!");
+        //while
+        bool sost = true;
+        while (sost == true)
+        {
+            //первое число
+            Console.WriteLine("Введите первое число.");
+            //захват ошибок
+            try
+            {
+                float a = float.Parse(Console.ReadLine());
+                //второе число
+                Console.WriteLine("Введите второе число");
+                float b = float.Parse(Console.ReadLine());
+                //действие
+                Console.WriteLine("Введите действие.");
+                string act = Console.ReadLine();
+                //финал
+                switch (act)
+                {
+                    case "+":
+                        {
+                            float c = a + b;
+                            Console.WriteLine(c);
+                        }
+                        break;
+                    case "-":
+                        {
+                            float c = a - b;
+                            Console.WriteLine(c);
+                        }
+                        break;
+                    case "*":
+                        {
+                            float c = a * b;
+                            Console.WriteLine(c);
+                        }
+                        break;
+                    case "/":
+                        {
+                            float c = a / b;
+                            Console.WriteLine(c);
+                        }
+                        break;
+                    default:
+                        {
+                            Console.WriteLine("Непредвиденная ошибка");
+                            sost = false;
+                        }
+                        break;
+                }
+            }
+            //реакция на ошибки
+            catch (Exception)
+            {
+                Console.WriteLine("Непредвиденная ошибка");
+                sost = false;
+            }
+        }
+        Console.ReadKey();
+    }
+}
